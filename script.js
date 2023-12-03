67909071801316e4d86b7861be91a0e35af5212e
Nikita <ambani-tech@outlook.com>
2023-12-03 20:57:05 +0300

    commit finished project

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.querySelector("#video");
+const button = document.querySelector("#button");
+
+// Prompt the user to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+    console.log("whoops, error here:", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset button
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
